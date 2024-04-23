0bee199ae88ba027b6265680db35586f7b290ba2
Menocas17 <rodolfomenocal7@gmail.com>
2024-04-23 11:53:27 -0600

    Playing around and experimenting with the inputs in C#

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        string Color;
         Console.WriteLine("Hello Sandbox World!!!!");
+        Console.Write("What is your favorite color? ");
+        Color = Console.ReadLine();
     }
 }
