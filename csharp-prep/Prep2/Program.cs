8c86c3069500cce5a26f0f0cbc0e5bd04d6554a0
Menocas17 <rodolfomenocal7@gmail.com>
2024-04-23 14:54:33 -0600

    Stretch challenges for preparation assignment 2 complet ed!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,6 +7,7 @@ class Program
     {   
 
         string gradeLetter;
+        string gradeSign;
 
         Console.WriteLine("What is your grade percentage?");
         string gradeInput = Console.ReadLine();
@@ -16,25 +17,83 @@ class Program
         if (gradePercentage >= 90)
         {
             gradeLetter = "A";
-            Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}");
+            int specificGrade = gradePercentage % 10;
+      
+            if (specificGrade < 3)
+            {
+                gradeSign = "-";
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}{gradeSign}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}");
+            }
         }
 
         else if (gradePercentage >= 80)
         {
             gradeLetter = "B";
-            Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}");
+            int specificGrade = gradePercentage % 10;
+            if (specificGrade >= 7)
+            {
+                gradeSign = "+";
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}{gradeSign}");
+            }
+            else if (specificGrade < 3)
+            {
+                gradeSign = "-";
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}{gradeSign}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}");
+            }
         }
 
         else if (gradePercentage >= 70)
         {
             gradeLetter = "C";
-            Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}");
+            int specificGrade = gradePercentage % 10;
+            if (specificGrade >= 7)
+            {
+                gradeSign = "+";
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}{gradeSign}");
+            }
+            else if (specificGrade < 3)
+            {
+                gradeSign = "-";
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}{gradeSign}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Congratulations!, you have passed the course with an {gradeLetter}");
+            }
         }
 
         else if (gradePercentage >= 60)
         {
             gradeLetter = "D";
-            Console.WriteLine($"You have not meet the required grade for passing the course, your grade was {gradeLetter}, please keep trying!");
+            int specificGrade = gradePercentage % 10;
+            if (specificGrade >= 7)
+            {
+                gradeSign = "+";
+                Console.WriteLine($"You have not meet the required grade for passing the course, your grade was {gradeLetter}{gradeSign}, please keep trying!");
+            }
+            else if (specificGrade < 3)
+            {
+                gradeSign = "-";
+                Console.WriteLine($"You have not meet the required grade for passing the course, your grade was {gradeLetter}{gradeSign}, please keep trying!");
+            }
+
+            else
+            {
+                Console.WriteLine($"You have not meet the required grade for passing the course, your grade was {gradeLetter}, please keep trying!");
+            }
+
+           
         }
 
         else if (gradePercentage < 60)
