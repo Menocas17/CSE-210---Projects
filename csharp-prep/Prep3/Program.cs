bdfe324738846f5aaeaee5966edced2f61caff52
Menocas17 <rodolfomenocal7@gmail.com>
2024-04-23 21:10:38 -0600

    Core and stretch requiriments for preparation activity 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,52 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        bool keepPLaying = true;
+        int attempts = 0;
+
+        while (keepPLaying == true)
+        {
+            Console.WriteLine("What is your guess?");
+            string stringGuess = Console.ReadLine ();
+            int numberGuess = int.Parse(stringGuess);
+
+            if (numberGuess == number)
+            {
+                Console.WriteLine($"You guessed it in {attempts} attempts!");
+                Console.WriteLine("Do you want to keep playing?");
+                string playAgain = Console.ReadLine().ToUpper();
+
+                if (playAgain == "YES")
+                {
+                    number = randomGenerator.Next(1, 101);
+                    attempts = 0;
+                }
+
+                else 
+                {
+                    keepPLaying = false;
+                }
+                
+            }
+
+            else if (numberGuess < number)
+            {
+                Console.WriteLine("Higher");
+                attempts += 1;
+            }
+
+            else if (numberGuess > number)
+            {
+                Console.WriteLine ("Lower");
+                attempts += 1;
+             }  
+        }
+
     }
 }
