2092465d90edd5d678f51a01080dccee910a91e0
Menocas17 <rodolfomenocal7@gmail.com>
2024-04-23 23:22:18 -0600

    Core and Stretch requiriments for prep 4 activity completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int> numbers = new List<int> ();
+        bool keepAsking = true;
+        int sumNumbers = 0;
+        int biggestNumber;
+        int smallestPositiveNumber = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        while (keepAsking == true)
+        {   
+            Console.Write("Enter a number:");
+            string stringNumber = Console.ReadLine();
+            int numberInput = int.Parse(stringNumber);
+            
+            numbers.Add(numberInput);
+
+            if (numberInput == 0)
+            {
+                keepAsking = false;
+
+                numbers.RemoveAt(numbers.Count - 1);
+
+                foreach (int number in numbers)
+                {
+                    sumNumbers += number;
+                }
+
+                int numbersCount = numbers.Count;
+                double avarage = (double)sumNumbers / numbersCount;
+                biggestNumber = numbers.Max();
+                numbers.Sort();
+
+                foreach (int number in numbers)
+                {
+                    if (number > 0)
+                    {
+                        smallestPositiveNumber = number;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"The sum of the numbers is: {sumNumbers}");
+                Console.WriteLine($"The avarage of the numbers is: {avarage}");
+                Console.WriteLine($"The largest number is: {biggestNumber}");
+                Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+                Console.WriteLine("The sorted list is:");
+
+                foreach(int number in numbers)
+                {
+                    Console.WriteLine($"{number}");
+                }
+            }
+        }
+        
     }
 }
