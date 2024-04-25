313e8829884b4ee3e78260e7295a30a83696c260
Menocas17 <rodolfomenocal7@gmail.com>
2024-04-25 12:13:28 -0600

    Preparation assignment 5 completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Reflection.Metadata;
+
+
+
+
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter you name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter you favorite number: ");
+        string userNumberInt = Console.ReadLine();
+        int userNumber = int.Parse(userNumberInt);
+
+        return userNumber;
+    }
+
+    static int SquareNumber(int userNumber)
+    {
+        int squaredNumber = userNumber * userNumber;
+
+        return squaredNumber;
+    }
+
+    static void DisplayResults(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber (userNumber);
+
+        DisplayResults (userName, squaredNumber);
     }
 }
+
