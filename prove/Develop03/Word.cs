14c5cf8df4cc20ff1a3629da8e736a877ebdd8f3
Menocas17 <rodolfomenocal7@gmail.com>
2024-05-11 21:28:20 -0600

    modified the program in order to meet the exceeding requiriments, add a new class 'Library', and modified the original design of the app to implement the new class, methos and attributes added to the classes, only missing the program class logic

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,27 @@
+public class Word 
+{
+    private string _word;
+    private bool _isHidden = false;
+
+
+    public void SetWord(string word)
+    {
+        _word = word;
+    }
+
+    public void Hide ()
+    {
+
+    }
+
+    public void Show ()
+    {
+
+    }
+
+    // public bool IsHidden ()
+    // {
+
+    // }
+
+}
