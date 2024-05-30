d260c584fae06aa33a6447dfb833aef4753ddff9
Menocas17 <rodolfomenocal7@gmail.com>
2024-05-29 21:35:28 -0600

    Adding the exceeding requiriments comments

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,3 +1,11 @@
+// Exceeding requirements:
+
+//     Add a verification step in order to verify if a goal was already completed before being able to record an event, and only display the uncompleted or eternal goals. 
+
+//     Add a verification step to the main menu so the user can only enter one of the available options
+
+//     Add a simple leveling system so the user can keep track of his lvl and xp.
+
 using System;
 
 class Program
