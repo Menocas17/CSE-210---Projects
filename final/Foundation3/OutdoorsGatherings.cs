a2f12048b533e7358aed4c86bea9801bf53e9760
Menocas17 <rodolfomenocal7@gmail.com>
2024-06-03 23:56:40 -0600

    Adding the OutdoorGatherings class with it's attributes and methods

---
+++ final/Foundation3/OutdoorsGatherings.cs
@@ -0,0 +1,22 @@
+public class OutdoorsGatherings : Event
+{
+    private string _weatherForecast;
+
+    public OutdoorsGatherings(string title, string description, string date, string time, Address address, string wheater) :base(title, description, date, time, address)
+    {
+        _weatherForecast = wheater;
+    }
+
+    public override void DisplayFullDetails()
+    {
+        base.DisplayFullDetails();
+        Console.WriteLine($"Type of event: Outdoor Gathering");
+        Console.WriteLine($"Statement of weather: {_weatherForecast}");
+    }
+
+    public override void ShortDescription()
+    {
+        Console.WriteLine($"Outdoor Gathering");
+        base.ShortDescription();
+    }
+}
