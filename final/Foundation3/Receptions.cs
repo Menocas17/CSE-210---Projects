1915a6fdd8b069073479b2ab4160d990d70d3acf
Menocas17 <rodolfomenocal7@gmail.com>
2024-06-03 23:48:40 -0600

    Adding the Receptions class with it's attributes and methods

---
+++ final/Foundation3/Receptions.cs
@@ -0,0 +1,28 @@
+public class Receptions : Event
+{
+    private string _recieptName;
+    private string _email;
+    private string _message;
+
+    public Receptions(string title, string description, string date, string time, Address address, string recieptName, string email, string message) :base(title, description, date, time, address)
+    {
+        _recieptName = recieptName;
+        _email = email;
+        _message = message;
+    }
+
+    public override void DisplayFullDetails()
+    {
+        base.DisplayFullDetails();
+        Console.WriteLine($"Type of event: Reception");
+        Console.WriteLine($"Email sent to guest:\n");
+        Console.WriteLine($"Reciept Name: {_recieptName}  -  Email address: {_email}");
+        Console.WriteLine($"{_message}");
+    }
+
+    public override void ShortDescription()
+    {
+        Console.WriteLine($"Reception");
+        base.ShortDescription();
+    }
+}
